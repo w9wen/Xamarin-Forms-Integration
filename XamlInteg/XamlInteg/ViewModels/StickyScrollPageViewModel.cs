8954a69401290b6c34bfe43709187c97e5e8a0bd
William (NB) <william.wen@gmail.com>
2019-05-31 11:49:33 +0800

    Frozen the view when scroling.

--- XamlInteg/XamlInteg/ViewModels/StickyScrollPageViewModel.cs
+++ XamlInteg/XamlInteg/ViewModels/StickyScrollPageViewModel.cs
@@ -3,14 +3,41 @@ using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Xamarin.Forms;
 
 namespace XamlInteg.ViewModels
 {
     public class StickyScrollPageViewModel : BindableBase
     {
+        Rectangle layoutBounds;
+        double scrollY;
+
+        public Rectangle LayoutBounds
+        {
+            get
+            {
+                return layoutBounds;
+            }
+            set
+            {
+                SetProperty(ref layoutBounds, value, () => RaisePropertyChanged());
+            }
+        }
+
+        public double ScrollY
+        {
+            get
+            {
+                return scrollY;
+            }
+        }
+
         public StickyScrollPageViewModel()
         {
-
+            layoutBounds.X = 0;
+            layoutBounds.Y = 240;
+            layoutBounds.Width = 360;
+            layoutBounds.Height = 36;
         }
     }
 }
--- XamlInteg/XamlInteg/Views/StickyScrollPage.xaml.cs
+++ XamlInteg/XamlInteg/Views/StickyScrollPage.xaml.cs
@@ -9,6 +9,18 @@ namespace XamlInteg.Views
         {
             InitializeComponent();
         }
+
+        private void Scroll_Pic_Scrolled(object sender, ScrolledEventArgs e)
+        {
+            if (e.ScrollY >= 240)
+            {
+                AbsoluteLayout.SetLayoutBounds(Label_Pic2, new Rectangle(0, e.ScrollY -5, 360, 36));
+            }
+            else
+            {
+                AbsoluteLayout.SetLayoutBounds(Label_Pic2, new Rectangle(0, 240, 360, 36));
+            }
+        }
     }
 
 
