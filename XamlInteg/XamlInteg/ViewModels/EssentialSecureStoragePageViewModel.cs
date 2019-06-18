6735fb353482e03ec4101d4d15c8b9d8fc7f6ddb
William (NB) <william.wen@gmail.com>
2019-06-19 04:35:57 +0800

    Formate the document.

--- XamlInteg/XamlInteg/ViewModels/EssentialSecureStoragePageViewModel.cs
+++ XamlInteg/XamlInteg/ViewModels/EssentialSecureStoragePageViewModel.cs
@@ -1,25 +1,51 @@
 ﻿using Newtonsoft.Json;
 using Prism.Commands;
-using Prism.Mvvm;
 using Prism.Navigation;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using Xamarin.Essentials;
 
 namespace XamlInteg.ViewModels
 {
+    /// <summary>
+    /// The Account Class.
+    /// </summary>
+    public class AccountModel
+    {
+        public string Account { get; set; }
+        public string CardNumber { get; set; }
+        public string Password { get; set; }
+    }
+
     public class EssentialSecureStoragePageViewModel : ViewModelBase
     {
+        #region Fields
+
+        /// <summary>
+        /// Store the account information.
+        /// </summary>
         private AccountModel _accountInfo = new AccountModel();
 
+        #endregion Fields
+
+        #region Properties
+
+        /// <summary>
+        /// Store the account information.
+        /// </summary>
         public AccountModel AccountInfo
         {
             get { return _accountInfo; }
             set { SetProperty(ref _accountInfo, value); }
         }
 
+        /// <summary>
+        /// Save command.
+        /// </summary>
         public DelegateCommand SaveCommand { get; }
+
+        #endregion Properties
+
+        #region Constructor
 
         public EssentialSecureStoragePageViewModel(INavigationService navigationService)
             : base(navigationService)
@@ -27,6 +53,13 @@ namespace XamlInteg.ViewModels
             SaveCommand = new DelegateCommand(SaveExecute);
         }
 
+        #endregion Constructor
+
+        #region Methods.
+
+        /// <summary>
+        /// Save execute.
+        /// </summary>
         private async void SaveExecute()
         {
             try
@@ -41,6 +74,10 @@ namespace XamlInteg.ViewModels
             }
         }
 
+        #endregion Methods.
+
+        #region Override.
+
         public override async void OnNavigatedTo(INavigationParameters parameters)
         {
             base.OnNavigatedTo(parameters);
@@ -54,12 +91,7 @@ namespace XamlInteg.ViewModels
                 // Possible that device doesn't support secure storage on device.
             }
         }
-    }
 
-    public class AccountModel
-    {
-        public string Account { get; set; }
-        public string CardNumber { get; set; }
-        public string Password { get; set; }
+        #endregion Override.
     }
 }
