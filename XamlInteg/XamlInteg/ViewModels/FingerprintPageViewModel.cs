0e04573fe27fa0ff054a760751a51aff9ac28a95
William (NB) <william.wen@gmail.com>
2019-09-17 08:40:03 +0800

    1. Try Fingerprint component. 2. Upgrade components.

--- XamlInteg/XamlInteg/ViewModels/FingerprintPageViewModel.cs
+++ XamlInteg/XamlInteg/ViewModels/FingerprintPageViewModel.cs
@@ -32,14 +32,70 @@ namespace XamlInteg.ViewModels
         {
             try
             {
-                var result = await CrossFingerprint.Current.AuthenticateAsync("請使用您的指紋進行識別");
-                if (result.Authenticated)
+                var type = await CrossFingerprint.Current.GetAuthenticationTypeAsync();
+
+                if (type == Plugin.Fingerprint.Abstractions.AuthenticationType.Fingerprint)
                 {
-                    await _pageDialogService.DisplayAlertAsync("識別結果", "認證合法", "Ok");
+                    await _pageDialogService.DisplayAlertAsync("本機認證", "使用指紋辨識", "Ok");
+                }
+                else if (type == Plugin.Fingerprint.Abstractions.AuthenticationType.Face)
+                {
+                    await _pageDialogService.DisplayAlertAsync("本機認證", "使用臉部辨識", "Ok");
                 }
                 else
                 {
-                    await _pageDialogService.DisplayAlertAsync("識別結果", "認證不合法", "Ok");
+                    await _pageDialogService.DisplayAlertAsync("本機認證", "無", "Ok");
+                }
+
+                var available = await CrossFingerprint.Current.GetAvailabilityAsync();
+
+                switch (available)
+                {
+                    case Plugin.Fingerprint.Abstractions.FingerprintAvailability.Available:
+                        break;
+
+                    case Plugin.Fingerprint.Abstractions.FingerprintAvailability.NoImplementation:
+                        break;
+
+                    case Plugin.Fingerprint.Abstractions.FingerprintAvailability.NoApi:
+                        break;
+
+                    case Plugin.Fingerprint.Abstractions.FingerprintAvailability.NoPermission:
+                        break;
+
+                    case Plugin.Fingerprint.Abstractions.FingerprintAvailability.NoSensor:
+                        break;
+
+                    case Plugin.Fingerprint.Abstractions.FingerprintAvailability.NoFingerprint:
+                        break;
+
+                    case Plugin.Fingerprint.Abstractions.FingerprintAvailability.Unknown:
+                        break;
+
+                    case Plugin.Fingerprint.Abstractions.FingerprintAvailability.Denied:
+                        break;
+
+                    default:
+                        break;
+                }
+
+                var isAvailable = await CrossFingerprint.Current.IsAvailableAsync();
+
+                if (isAvailable)
+                {
+                    var result = await CrossFingerprint.Current.AuthenticateAsync("請使用您的指紋進行識別");
+                    if (result.Authenticated)
+                    {
+                        await _pageDialogService.DisplayAlertAsync("識別結果", "認證合法", "Ok");
+                    }
+                    else
+                    {
+                        await _pageDialogService.DisplayAlertAsync("識別結果", "認證不合法", "Ok");
+                    }
+                }
+                else
+                {
+                    await _pageDialogService.DisplayAlertAsync("識別結果", "無設定生物辨識", "Ok");
                 }
             }
             catch (Exception ex)
