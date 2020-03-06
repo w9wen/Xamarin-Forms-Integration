e7c9e24557bf66e681b83042da744e34dbe31ee0
William (NB) <william.wen@gmail.com>
2020-03-06 11:21:57 +0800

    [Renderer] iOS 使用 WkWebView

---
+++ XamlInteg/XamlInteg.iOS/Renderers/WebViewControlRenderer.cs
@@ -0,0 +1,16 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using XamlInteg.Controls;
+using XamlInteg.iOS.Renderers;
+
+[assembly: ExportRenderer(typeof(WebViewControl), typeof(WebViewControlRenderer))]
+
+namespace XamlInteg.iOS.Renderers
+{
+    public class WebViewControlRenderer : WkWebViewRenderer
+    {
+        public WebViewControlRenderer()
+        {
+        }
+    }
+}
